cf9665ce72ee4af6fa4af7353be02e8cad78e1ed
Ajanzz <128408144+Ajanzz@users.noreply.github.com>
2023-06-22 14:23:21 -0700

    Create Quadrilateral.java

---
+++ Quadrilateral.java
@@ -0,0 +1,17 @@
+public abstract class Quadrilateral {
+    protected double side1;
+    protected double side2;
+    protected double side3;
+    protected double side4;
+
+    public Quadrilateral(double side1, double side2, double side3, double side4) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+        this.side4 = side4;
+    }
+
+    public abstract double calculateArea();
+
+    public abstract double calculatePerimeter();
+}
